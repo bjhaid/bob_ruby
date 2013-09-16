0718c3a9e09ccd2c730cb4648c09fb16d3e8a8a3
Abejide Ayodele <abejideayodele@gmail.com>
2013-09-16 08:43:52 -0500

    added bob class

--- spec/bob_spec.rb
+++ spec/bob_spec.rb
@@ -1,8 +1,8 @@
 require 'spec_helper'
 
 describe Bob do
-  it "should say Sure when you ask him a question 'Bob how are you?'"
-  it "should say Sure when you ask him a question 'Bob are you mad?'"
-  it "should say Sure when you ask him a question 'Bob how old are you?'"
-  it "should say Sure when you ask him a question 'Bob what's going on?'"
+  it "should say 'Sure' when you ask him a question 'Bob how are you?'"
+  it "should say 'Sure' when you ask him a question 'Bob are you mad?'"
+  it "should say 'Sure' when you ask him a question 'Bob how old are you?'"
+  it "should say 'Sure' when you ask him a question 'Bob what's going on?'"
 end
